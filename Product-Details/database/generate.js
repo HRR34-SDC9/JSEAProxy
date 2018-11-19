676461fb0768255611dd9878bdd5d2e0990e49cb
JSEA <julius.sea@mac.com>
2018-11-19 02:44:05 -0500

    refactored data generation script using node.js streams

--- Product-Details/database/generate.js
+++ Product-Details/database/generate.js
@@ -1,96 +1,145 @@
 const faker = require("faker");
 const fs = require('fs');
 const path = require('path');
+const { Readable } = require('stream');
 const stringify = require("csv-stringify");
+const generate = require("csv-generate");
 
-
-const capitalizeFirst = str => {
-  let capitalized = str[0].toUpperCase();
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === " ") {
-      capitalized += str[i].toUpperCase();
-    } else {
-      capitalized += str[i];
+  const capitalizeFirst = str => {
+    let capitalized = str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++) {
+      if (str[i - 1] === " ") {
+        capitalized += str[i].toUpperCase();
+      } else {
+        capitalized += str[i];
+      }
     }
-  }
-  return capitalized;
-};
+    return capitalized;
+  };
 
-const randomNum = () => Math.floor(Math.random() * Math.floor(100));
+  const randomNum = () => Math.floor(Math.random() * Math.floor(100));
 
-const populateImages = index => {
-  const images = [
-    {
-      image: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${index}-min.jpg`,
-      color: capitalizeFirst(faker.commerce.color())
+  const populateImages = index => {
+    const images = [
+      {
+        image: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${index}-min.jpg`,
+        color: capitalizeFirst(faker.commerce.color())
+      }
+    ];
+    let numOfImages;
+
+    for (let i = 4; i > 0; i -= 1) {
+      if (index % i === 0) {
+        numOfImages = i - 1;
+        break;
+      }
     }
-  ];
-  let numOfImages;
-
-  for (let i = 4; i > 0; i -= 1) {
-    if (index % i === 0) {
-      numOfImages = i - 1;
-      break;
-    }
-  }
-  for (let i = 1; i <= numOfImages; i++) {
-    images.push({
-      image: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${randomNum()}-min.jpg`,
-      color: capitalizeFirst(faker.commerce.color())
-    });
-  }
-  return images;
-};
-
-const products = [];
-let count = 1;
-
-const createMockProducts = () => {
-  console.time('time to generate');
-    for (var i = 1; i <= 1; i++) {
-      count++
-      products.push({
-        _id: count,
-        name: faker.commerce.productName(),
-        rating: Number(faker.finance.amount(1, 5, 1)),
-        reviewCount: faker.random.number({ min: 20, max: 150 }),
-        itemNum: count,
-        price: faker.commerce.price(50, 500),
-        mainImage: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${faker.random.number({ min: 1, max: 100 })}-min.jpg`,
-        images: populateImages(count)
+    for (let i = 1; i <= numOfImages; i++) {
+      images.push({
+        image: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${randomNum()}-min.jpg`,
+        color: capitalizeFirst(faker.commerce.color())
       });
     }
-  return products;
-};
+    return images;
+  };
 
-stringify(createMockProducts(), {
-  // header: true,
-  // columns: ['_id', 'name', 'rating', 'reviewCount', 'itemNum', 'price', 'mainImage', 'images']
-}, (error, output) => {
-  // return new Promise((resolve, reject) => {
-    let writeStream = fs.createWriteStream(__dirname + '/data.csv');
-    let j = 0
+  // const products = [];
 
-    writeStream.on('drain', function () {
-      console.log('drain', j)
-      console.log('count', count)
-      write();
-    });
-    write();
+  /*
+  When the read method is called on a readable stream, the implementation can push partial data to the queue. For example, we can push one product at a time, starting with the header, and incrementing that on every push. The read method will continue to fire, and we’ll push more products. We need to stop this cycle somewhere, and that’s why an if statement to push null when the count is equal to the endCount.
+  */
 
-    function write() {
-      while (j <= 49) {
-        j++
-        if (!writeStream.write(output)) {
-          return;
-        }
+  let count = 1;
+  let endCount = 100;
+  let divisor = 10;
+
+  const writeStream = fs.createWriteStream(__dirname + '/data.csv');
+
+  const incomingData = new Readable({
+    // objectMode: true,
+
+    read() {
+      if (count === 1) {
+        console.time(`time to generate these rows`);
+        this.push('_id, name, rating, reviewCount, itemNum, price, mainImage, images\n');
       }
-        writeStream.end();
-        writeStream.on('finish', () => {console.timeEnd('time to generate')});
-        writeStream.on('error', () => {console.error(error)});
+
+      this.push(`${count}, ${faker.commerce.productName()}, ${faker.finance.amount(1, 5, 1)}, ${faker.random.number({min: 20, max: 150})},${count}, ${faker.commerce.price(50, 500)}, https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${faker.random.number({ min: 1, max: 100 })}-min.jpg, ${JSON.stringify(populateImages(count))}\n`);
+
+      count++
+      if (count === endCount) {
+        this.push(null)
+      }
+      if (count % divisor === 0) {
+        console.log(`${count} rows have been created`);
+        console.timeEnd(`time to generate these rows`);
+        console.time(`time to generate these rows`);
+      }
     }
-  // })
-});
+  });
+
+  incomingData.pipe(writeStream).on('finish', () => {console.log('file generation complete')}).on('error', () => {console.error(error)});
+
+  // const createMockProducts = () => {
+  //   console.time('time to generate');
+  //     for (var i = 1; i <= 5; i++) {
+  //       count++
+  //       products.push({
+  //         _id: count,
+  //         name: faker.commerce.productName(),
+  //         rating: Number(faker.finance.amount(1, 5, 1)),
+  //         reviewCount: faker.random.number({ min: 20, max: 150 }),
+  //         itemNum: count,
+  //         price: faker.commerce.price(50, 500),
+  //         mainImage: `https://s3-us-west-1.amazonaws.com/hrr34-trailblazer/${faker.random.number({ min: 1, max: 100 })}-min.jpg`,
+  //         images: populateImages(i)
+  //       });
+  //     }
+  //   return products;
+  // };
+
+  // streamCSV.pipe(writeStream);
+  // streamCSV.write(createMockProducts());
+  // console.log(products);
+  // streamCSV.end();
+
+  // writeStream.on('finish', () => { resolve(console.timeEnd('time to generate')) });
+  // writeStream.on('error', () => {console.error(error)});
+
+// });
+
+
+
+// stringify(createMockProducts(), {
+//   header: true,
+//   columns: ['_id', 'name', 'rating', 'reviewCount', 'itemNum', 'price', 'mainImage', 'images']
+// }, (error, output) => {
+//   // return new Promise((resolve, reject) => {
+//     let writeStream = fs.createWriteStream(__dirname + '/data.csv');
+//     let j = 0
+
+//     writeStream.once('drain', function () {
+//       console.log('drain', j)
+//       console.log('count', count)
+//       write();
+//     });
+//     write();
+
+//     function write() {
+//       while (j < 1) {
+//         j++
+//         if (!writeStream.write(output)) {
+//           return;
+//         }
+//       }
+//         writeStream.end();
+//         writeStream.on('finish', () => {console.timeEnd('time to generate')});
+//         writeStream.on('error', () => {console.error(error)});
+//     }
+//   // })
+// });
+
+module.exports = generate;
 
 
 
